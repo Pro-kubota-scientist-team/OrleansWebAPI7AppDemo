2f8ab601afced4a8af1b48942852dec3998cb010
y <KTC2243020002@edu.kyoto-tech.ac.jp>
2024-01-29 15:16:34 +0900

    create Jouranal file

---
+++ Controllers/Journal/JournalController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrleansWebAPI7AppDemo.Models.Journal;
+using OrleansWebAPI7AppDemo.Orleans.Abstractions;
+
+namespace OrleansWebAPI7AppDemo.Controllers.Journal
+{
+    [ApiController]
+    [Route("Journal/[controller]")]
+    public class JournalController : ControllerBase
+    {
+
+        private readonly IGrainFactory _grains;
+
+        public JournalController(IGrainFactory grains)
+        {
+            _grains = grains;
+        }
+        /// <summary>
+        /// 勘定科目を取得します
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        [Route("")]
+        public IList<string> Index()
+        {
+            var items = new List<string>();
+            // ↓↓　一般的にはデータベースから取得する
+            // SELECT * FROM AccountItem;
+            items.Add("100");
+            items.Add("700");
+            // ↑↑　
+            return items;
+        }
+
+        /// <summary>
+        /// 指定したコードの勘定科目を取得します
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        [Route("{id}")]
+
+        public async Task<IActionResult> Get(String id)
+        {
+            // グレインの呼び出し
+            var campanyGrain = _grains.GetGrain<IAccountItemGrain>(id);
+            // 指定グレインのGETメソッドを実行して結果を取得する
+            var accountItem = await campanyGrain.Get();
+            if (accountItem == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(accountItem);
+            }
+        }
+    }
+}
